0667b8d81021f05be8b5831e3bc64183fce886fe
ramakrishnaf7 <153580984+ramakrishnaf7@users.noreply.github.com>
2024-08-18 16:16:31 +0530

    Update server.js

--- Backend-Tracker/server.js
+++ Backend-Tracker/server.js
@@ -17,11 +17,11 @@ const app = express()
 
 //static files
 
-app.use(express.static(path.join(__dirname, './client/build')))
+// app.use(express.static(path.join(__dirname, './client/build')))
 
-app.get('*',function (req,res) {
-    res.sendFile(path.join(__dirname,'./client/build/index.html'));    
-})
+// app.get('*',function (req,res) {
+//     res.sendFile(path.join(__dirname,'./client/build/index.html'));    
+// })
 //middlewares
 app.use(morgan('dev'))
 app.use(express.json())
